7d77ed4cb6aadd9686bfb144c93f5f4e94e8c167
ChristopherMcElroy <csmcelroy@me.com>
2021-06-15 15:16:58 -0400

    26*

--- aoc2016/day13.swift
+++ aoc2016/day13.swift
@@ -8,7 +8,48 @@
 import Foundation
 
 func day13() {
-    let input = inputLines(13)
+    let input = inputInts(13)[0]
     
-    print(input)
+    var history: Set<C2> = []
+    var options: Set<C2> = [C2(1, 1)]
+    var steps = 0
+    
+    var a1 = 0
+    var a2 = 0
+    
+    func isOpen(_ c: C2) -> Bool {
+        if c.x < 0 || c.y < 0 { return false }
+        
+        var n = c.x*c.x + 3*c.x + 2*c.x*c.y + c.y + c.y*c.y + input
+        var ones = 0
+        
+        while n > 0 {
+            ones += n % 2
+            n = n/2
+        }
+        
+        return ones % 2 == 0
+    }
+    
+    while steps < 50 || !history.contains(C2(31, 39)) {
+        steps += 1
+        var newOptions: Set<C2> = []
+        
+        for var option in options {
+            for adj in option.adjacents {
+                if isOpen(adj) {
+                    if history.insert(adj).inserted {
+                        newOptions.insert(adj)
+                    }
+                }
+            }
+        }
+        
+        options = newOptions
+        
+        if history.contains(C2(31, 39)) && a1 == 0 { a1 = steps }
+        if steps == 50 { a2 = history.count }
+    }
+    
+    print(a1, a2)
 }
